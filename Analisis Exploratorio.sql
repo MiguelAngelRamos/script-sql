11694a6ef848e9160f0a221a82ce4196f421a4bc
Miguel Ramos <return.infox@gmail.com>
2023-04-11 09:50:10 -0400

    analisis exploratorio

--- Analisis Exploratorio.sql
+++ Analisis Exploratorio.sql
@@ -17,7 +17,30 @@ SELECT AVG(total_pagado) FROM Cliente; -- OBTENEMOS EL PROMEDIO DEL TOTAL PAGADO
 SELECT * FROM Cliente WHERE total_pagado > (SELECT AVG(total_pagado) FROM Cliente);
 
 -- 4. Para indicar cuantos vendedores tienen un salario inferior al promedio
-
+-- SELECT * FROM Vendedor WHERE salario < (SELECT AVG(salario) FROM vendedor);
+   SELECT COUNT(*) FROM Vendedor WHERE salario < (SELECT AVG(salario) FROM vendedor);
+   
 -- 5. Para indicar cuantos productos son más baratos que el promedio
-
+--  SELECT * FROM Producto WHERE Precio < (SELECT AVG(Precio) FROM producto); 
+--- SELECT * FROM Producto;
+   SELECT COUNT(*) FROM Producto WHERE Precio < (SELECT AVG(Precio) FROM producto); 
+  
 -- 6. Para seleccionar el nombre, el apellido de los vendedores que tienen un salario superior al promedio.
+   SELECT nombre, apellidos FROM Vendedor WHERE Salario > (SELECT AVG(Salario) FROM Vendedor);
+   
+-- 7. Para seleccionar el producto más barato y el producto más caro del inventario.
+
+  -- Pistas Select MIN() y MAX()
+  -- SELECT * FROM producto WHERE precio = (SELECT MIN(precio) FROM producto) OR precio = (SELECT MAX(precio) FROM producto);
+  SELECT MIN(Precio) AS min_precio, MAX(Precio) AS max_precio FROM Producto;
+  
+-- 8. Sumatoria de todos los productos
+  SELECT SUM(Precio) AS costo_total FROM Producto;
+  
+-- 9. GROUP BY, ORDER BY
+
+-- 10.
+
+-- 11.
+  
+  
