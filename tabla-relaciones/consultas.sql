f6baa28c810eb014f939db3b5aebaa72731d97cc
Miguel Ramos <return.infox@gmail.com>
2023-04-12 09:52:21 -0400

    group by

--- tabla-relaciones/consultas.sql
+++ tabla-relaciones/consultas.sql
@@ -21,4 +21,21 @@ SELECT c.* FROM clientes c JOIN cuentas cu ON c.cliente_id = cu.cliente_id WHERE
 
     SELECT c.* FROM clientes c JOIN cuentas cu ON c.cliente_id = cu.cliente_id
 	WHERE c.correo_electronico LIKE '%gmail%';
+    
+-- 6. Agregar mas clientes  
+  INSERT INTO clientes (cliente_id, nombre, apellido, correo_electronico, direccion) VALUES
+  (5, 'Laura', 'González', 'lauragonzalez@gmail.com', 'Calle 1, Ciudad'),
+  (6, 'Carlos', 'Martínez', 'carlosmartinez@hotmail.com', 'Calle 2, Ciudad'),
+  (7, 'Marta', 'Fernández', 'martafdez@gmail.com', 'Calle 3, Ciudad'),
+  (8, 'Jorge', 'Rojas', 'jorgerojas@yahoo.com', 'Calle 3, Ciudad'),
+  (9, 'Juan', 'Hernández', NULL, 'Calle 1, Ciudad'),
+  (10, 'Laura', 'García', NULL, 'Calle 2, Ciudad');
   
+  /*
+  GROUP BY en SQL se utiliza para agrupar filas con valores idénticos y realizar cálculos en cada grupo resultante
+  */
+  
+  -- Si queremos saber cuántos clientes hay por cada dirección
+  SELECT direccion, COUNT(cliente_id) AS cantidad_de_clientes FROM clientes GROUP BY direccion;
+  -- CANTIDAD DE CORREO
+  SELECT COUNT(correo_electronico) FROM clientes;
